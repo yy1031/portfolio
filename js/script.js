d437694dfe805d21e9de1520418063b937d57517
yukako <tomtomko.0310@gmail.com>
2024-06-19 16:18:36 +0900

    ポートフォリオサイトの作成完了

--- js/script.js
+++ js/script.js
@@ -2,31 +2,6 @@ $(function(){
 
    new WOW().init();
 
- 
-   $(".work-item a").on('click', function(){
-      var imgSrc = $(this).children('.work-img').attr('src');
-      var imgSrcSp = $(this).children('.work-img-sp').attr('src');
-      var url = $(this).attr('href');
-      $(".modal-img").children().attr('src', imgSrc);
-      $(".modal-img-sp").children().attr('src', imgSrcSp);
-      if(!url.length){
-         $(".modal-btn").removeClass('is-active');
-         
-      } else{
-         $(".modal-btn").addClass('is-active');
-         $(".modal-btn").children().attr('href', url);
-      }
-      $(".modal").fadeIn();
-      $('body,html').css('overflow-y', 'hidden');
-      return false;
-   });
-
-   $(".modal-overlay").click(function(){
-      $('.modal').fadeOut(200);
-      $('body,html').css('overflow-y', 'visible');
-      return false;
-   });
-
    jQuery('a[href^="#"]').on('click', function(){
 
       var header = jQuery('header').innerHeight();
@@ -38,7 +13,6 @@ $(function(){
       jQuery('html, body').animate({
         scrollTop: position
       },
-      300)
+      400)
     });
 })
-
